dcea0074d5a8f60c40c1221e48aed417317d624e
Mansoon Paudel <mansoonpaudel@gmail.com>
2026-05-03 10:41:14 +0545

    Bug Fixes

--- My project (2)/Assets/Scripts/Health/Health.cs
+++ My project (2)/Assets/Scripts/Health/Health.cs
@@ -62,4 +62,8 @@ public class Health : MonoBehaviour
         Physics2D.IgnoreLayerCollision(10, 11, false);
         invulnerable = false;
     }
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
 }
